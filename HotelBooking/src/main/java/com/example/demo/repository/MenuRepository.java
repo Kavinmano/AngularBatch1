fb3059c79b179e93ac34336993c6a2fbc3c8f10b
Kavin Kumar K <110532169+Kavinmano@users.noreply.github.com>
2025-06-20 12:32:11 +0530

    Add files via upload

---
+++ HotelBooking/src/main/java/com/example/demo/repository/MenuRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+    List<Menu> findByRestaurantId(Long restaurantId);
+}
